b042cb1c819e654ebe07bfc2ecf1f1b403b8b2c7
Sohit Kushwaha <65173131+itsmesohit@users.noreply.github.com>
2024-07-19 14:31:11 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -15,12 +15,6 @@ class TaskBase(BaseModel):
     assigned_to: str
     progress: ProgressEnum = ProgressEnum.not_started_yet
 
-    @validator('due_date')
-    def due_date_in_future(cls, v):
-        if v <= datetime.now():
-            raise ValueError('Due date must be in the future')
-        return v
-
 class TaskCreate(TaskBase):
     pass
 
